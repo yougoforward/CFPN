abb83d8eeb203abb2b793c3bcaba57746dbeee85
yougoforward <908865817@qq.com>
2020-08-30 00:05:03 +0800

    8

--- encoding/models/__init__.py
+++ encoding/models/__init__.py
@@ -12,6 +12,7 @@ from .dfpn8_gsf import *
 from .dfpn82_gsf import *
 from .dfpn83_gsf import *
 from .dfpn84_gsf import *
+from .dfpn85_gsf import *
 
 def get_segmentation_model(name, **kwargs):
     from .fcn import get_fcn
@@ -27,6 +28,8 @@ def get_segmentation_model(name, **kwargs):
         'dfpn82_gsf': get_dfpn82_gsf,
         'dfpn83_gsf': get_dfpn83_gsf,
         'dfpn84_gsf': get_dfpn84_gsf,
+        'dfpn85_gsf': get_dfpn85_gsf,
+
 
     }
     return models[name.lower()](**kwargs)
