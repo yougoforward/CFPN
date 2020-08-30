a224ba82a39dffab0beb366012fd56670c3f247b
yougoforward <908865817@qq.com>
2020-08-30 23:22:12 +0800

    8

--- encoding/models/dfpn84_gsf.py
+++ encoding/models/dfpn84_gsf.py
@@ -68,7 +68,7 @@ class dfpn84_gsfHead(nn.Module):
                                    norm_layer(inter_channels), nn.ReLU())
         self.context2 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
 
-        self.project = nn.Sequential(nn.Conv2d(5*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+        self.project = nn.Sequential(nn.Conv2d(8*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
@@ -86,15 +86,15 @@ class dfpn84_gsfHead(nn.Module):
         # out2 = self.gff3(out2)
         cat2, p2_1, p2_8=self.context2(out2)
         
-        # p4_1 = F.interpolate(p4_1, (h,w), **self._up_kwargs)
-        # p4_2 = F.interpolate(p4_2, (h,w), **self._up_kwargs)
-        # p4_4 = F.interpolate(p4_4, (h,w), **self._up_kwargs)
-        # p4_8 = F.interpolate(p4_8, (h,w), **self._up_kwargs)
-        p4 = F.interpolate(p4, (h,w), **self._up_kwargs)
+        p4_1 = F.interpolate(p4_1, (h,w), **self._up_kwargs)
+        p4_2 = F.interpolate(p4_2, (h,w), **self._up_kwargs)
+        p4_4 = F.interpolate(p4_4, (h,w), **self._up_kwargs)
+        p4_8 = F.interpolate(p4_8, (h,w), **self._up_kwargs)
+        # p4 = F.interpolate(p4, (h,w), **self._up_kwargs)
         p3_1 = F.interpolate(p3_1, (h,w), **self._up_kwargs)
         p3_8 = F.interpolate(p3_8, (h,w), **self._up_kwargs)
-        # out = self.project(torch.cat([p2_1,p2_8,p3_1,p3_8,p4_1,p4_2,p4_4,p4_8], dim=1))
-        out = self.project(torch.cat([p2_1,p2_8,p3_1,p3_8,p4], dim=1))
+        out = self.project(torch.cat([p2_1,p2_8,p3_1,p3_8,p4_1,p4_2,p4_4,p4_8], dim=1))
+        # out = self.project(torch.cat([p2_1,p2_8,p3_1,p3_8,p4], dim=1))
         # cat4 = F.interpolate(cat4, (h,w), **self._up_kwargs)
         # cat3 = F.interpolate(cat3, (h,w), **self._up_kwargs)
         # out = self.project(torch.cat([cat2, cat3, cat4], dim=1))
