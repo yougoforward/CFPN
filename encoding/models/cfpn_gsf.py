3b018b303645b73f19e123a1a260a7e58f1b7ae5
yougoforward <908865817@qq.comm>
2020-09-15 10:17:10 +0800

    cfpn_gsf

--- encoding/models/cfpn_gsf.py
+++ encoding/models/cfpn_gsf.py
@@ -181,14 +181,53 @@ def get_cfpn_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
     return model
 
 
+# class PAM_Module(nn.Module):
+#     """ Position attention module"""
+#     #Ref from SAGAN
+#     def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer):
+#         super(PAM_Module, self).__init__()
+#         self.chanel_in = in_dim
+#         self.pool = nn.MaxPool2d(kernel_size=4)
+
+#         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+#         self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
+
+#         self.gamma = nn.Sequential(nn.Conv2d(in_channels=in_dim, out_channels=1, kernel_size=1, bias=True), nn.Sigmoid())
+
+#         self.softmax = nn.Softmax(dim=-1)
+
+
+#     def forward(self, x):
+#         """
+#             inputs :
+#                 x : input feature maps( B X C X H X W)
+#             returns :
+#                 out : attention value + input feature
+#                 attention: B X (HxW) X (HxW)
+#         """
+#         xp = self.pool(x)
+#         m_batchsize, C, height, width = x.size()
+#         m_batchsize, C, hp, wp = xp.size()
+#         proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+#         proj_key = self.key_conv(xp).view(m_batchsize, -1, wp*hp)
+#         energy = torch.bmm(proj_query, proj_key)
+#         attention = self.softmax(energy)
+#         proj_value = xp.view(m_batchsize, -1, wp*hp)
+        
+#         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+#         out = out.view(m_batchsize, C, height, width)
+
+#         gamma = self.gamma(x)
+#         out = (1-gamma)*out + gamma*x
+#         return out
+
 class PAM_Module(nn.Module):
     """ Position attention module"""
     #Ref from SAGAN
     def __init__(self, in_dim, key_dim, value_dim, out_dim, norm_layer):
         super(PAM_Module, self).__init__()
         self.chanel_in = in_dim
-        self.pool = nn.MaxPool2d(kernel_size=4)
-
+        self.pool = nn.MaxPool2d(kernel_size=2)
         self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
         self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=key_dim, kernel_size=1)
 
@@ -205,6 +244,9 @@ class PAM_Module(nn.Module):
                 out : attention value + input feature
                 attention: B X (HxW) X (HxW)
         """
+        _,_,h,w = x.size()
+        x0 = x
+        x = self.pool(x)
         xp = self.pool(x)
         m_batchsize, C, height, width = x.size()
         m_batchsize, C, hp, wp = xp.size()
@@ -216,8 +258,9 @@ class PAM_Module(nn.Module):
         
         out = torch.bmm(proj_value, attention.permute(0, 2, 1))
         out = out.view(m_batchsize, C, height, width)
+        
+        out = F.interpolate(out, (h,w), mode='bilinear', align_corner=True)
 
-        gamma = self.gamma(x)
-        out = (1-gamma)*out + gamma*x
+        gamma = self.gamma(x0)
+        out = (1-gamma)*out + gamma*x0
         return out
-
