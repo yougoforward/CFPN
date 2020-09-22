f634ecc96a47da7b840bcedc8d9d707865f4de0d
yougoforward <908865817@qq.com>
2020-09-22 10:34:18 +0800

    cfpn

--- experiments/segmentation/scripts/vggnet_pcontext.sh
+++ experiments/segmentation/scripts/vggnet_pcontext.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train_scratch --dataset pcontext \
-    --model vggnet --base-size 512 --crop-size 512 \
+    --model vggnet --base-size 320 --crop-size 320 \
     --checkname vggnet_pcontext --epochs 150
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset pcontext \
-    --model vggnet --base-size 512 --crop-size 512 \
+    --model vggnet --base-size 320 --crop-size 320 \
     --resume experiments/segmentation/runs/pcontext/vggnet/vggnet_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset pcontext \
-    --model vggnet --base-size 512 --crop-size 512 \
+    --model vggnet --base-size 320 --crop-size 320 \
     --resume experiments/segmentation/runs/pcontext/vggnet/vggnet_pcontext/model_best.pth.tar --split val --mode testval --ms
