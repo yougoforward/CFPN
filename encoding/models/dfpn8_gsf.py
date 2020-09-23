5885354ce5c491fa26361ba78ec748c6c1c5bc24
yougoforward <908865817@qq.com>
2020-09-23 09:05:02 +0800

    cfpn

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -53,9 +53,7 @@ class dfpn8_gsfHead(nn.Module):
                             nn.Sigmoid())
         self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
-        self.conv6 = nn.Sequential(nn.Conv2d(2*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(inter_channels),
-                                   nn.ReLU(),
+        self.conv6 = nn.Sequential(
                                    nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
 
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
@@ -99,7 +97,8 @@ class dfpn8_gsfHead(nn.Module):
         out = self.gff(out)
 
         #
-        out = torch.cat([out, gp.expand_as(out)], dim=1)
+        # out = torch.cat([out, gp.expand_as(out)], dim=1)
+        out = out+gp
 
         return self.conv6(out)
 
