d8d28659deb08c1a0d980fed93ab16f0f5af2ace
yougoforward <908865817@qq.com>
2020-09-24 08:32:51 +0800

    cfpn

--- encoding/models/dfpn85_gsf.py
+++ encoding/models/dfpn85_gsf.py
@@ -48,53 +48,71 @@ class dfpn85_gsfHead(nn.Module):
                             nn.Conv2d(in_channels, inter_channels, 1, bias=False),
                             norm_layer(inter_channels),
                             nn.ReLU(True))
-        self.se = nn.Sequential(nn.AdaptiveAvgPool2d(1),
-                            nn.Conv2d(6*inter_channels, inter_channels, 1, bias=False),
-                            nn.ReLU(True),
+        self.se = nn.Sequential(
                             nn.Conv2d(inter_channels, inter_channels, 1, bias=True),
                             nn.Sigmoid())
         self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
         self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
+        self.s_localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
+        self.s_localUp4=localUp(512, inter_channels, norm_layer, up_kwargs)
+        
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
-
-        self.context4 = Context(in_channels, inter_channels, inter_channels, 8, norm_layer)
         self.project4 = nn.Sequential(nn.Conv2d(2*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels), nn.ReLU())
-        self.context3 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
-        self.project3 = nn.Sequential(nn.Conv2d(2*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(inter_channels), nn.ReLU())
-        self.context2 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
+        
 
         self.project = nn.Sequential(nn.Conv2d(6*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
+        self.dconv0 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.dconv1 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=2, dilation=2, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.dconv2 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=4, dilation=4, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.dconv3 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=8, dilation=8, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.dconv4 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=16, dilation=16, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.dconv5 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=32, dilation=32, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        
+        
+        self.skip4 = nn.Sequential(nn.Conv2d(2048, inter_channels, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        self.skip3 = nn.Sequential(nn.Conv2d(1024, inter_channels, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(inter_channels), nn.ReLU())
+        
+        
+        
     def forward(self, c1,c2,c3,c4):
         _,_, h,w = c2.size()
-        cat4, p4_1, p4_8=self.context4(c4)
-        p4 = self.project4(cat4)
-                
-        out3 = self.localUp4(c3, p4)
-        cat3, p3_1, p3_8=self.context3(out3)
-        p3 = self.project3(cat3)
         
-        out2 = self.localUp3(c2, p3)
-        cat2, p2_1, p2_8=self.context2(out2)
+        s4 = self.skip(c4)
+        s3 = self.skip(c3)
+        sp3 = self.s_localUp3(c2, s3)
+        sp4 = self.s_localUp4(c2, s4)
         
-        p4_1 = F.interpolate(p4_1, (h,w), **self._up_kwargs)
-        p4_8 = F.interpolate(p4_8, (h,w), **self._up_kwargs)
-        p3_1 = F.interpolate(p3_1, (h,w), **self._up_kwargs)
-        p3_8 = F.interpolate(p3_8, (h,w), **self._up_kwargs)
-        cat2 = torch.cat([p2_1,p2_8,p3_1,p3_8,p4_1,p4_8], dim=1)
-        out = self.project(cat2)
+        p3 = self.localUp4(c3, s4)
+        p2 = self.localUp3(c2, p3)
+        feat = self.project4(torch.cat([p2,sp4], dim=1))
+        
+        f0 = self.dconv0(feat)
+        f1 = self.dconv1(feat)
+        f2 = self.dconv2(feat)
+        f3 = self.dconv3(feat)
+        f4 = self.dconv4(feat)
+        f5 = self.dconv5(feat)
+        out = self.project(torch.cat([f0,f1,f2,f3,f4,f5], dim=1))
 
         #gp
         gp = self.gap(c4)    
-        # # se
-        se = self.se(cat2)
+        # se
+        se = self.se(gp)
         out = out + se*out
         out = self.gff(out)
 
