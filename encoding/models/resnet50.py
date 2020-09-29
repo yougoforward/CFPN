fbb28b708ff9428845eb0c3038c71d26679c88c8
yougoforward <908865817@qq.comm>
2020-09-29 23:42:51 +0800

    res50

--- encoding/models/resnet50.py
+++ encoding/models/resnet50.py
@@ -16,7 +16,8 @@ up_kwargs = {'mode': 'bilinear', 'align_corners': True}
 class resnet50(nn.Module):
     def __init__(self, nclass, backbone, aux=True, se_loss=False, norm_layer=nn.BatchNorm2d, base_size=520, crop_size=480, mean=[.485, .456, .406],
                  std=[.229, .224, .225], **kwargs):
-        super(resnet50, self).__init__(nclass, backbone, aux, se_loss, norm_layer=norm_layer, **kwargs)
+        super(resnet50, self).__init__()
+        self.aux = aux
         self.mean = mean
         self.std = std
         self.base_size = base_size
