d428be2285c68f3c0d1c17df5c707de850ffa4b7
yougoforward <908865817@qq.com>
2020-09-30 14:57:51 +0800

    cfpn

--- encoding/models/dfpn86_gsf.py
+++ encoding/models/dfpn86_gsf.py
@@ -55,7 +55,7 @@ class dfpn86_gsfHead(nn.Module):
 
         self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
 
-        self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
+        self.localUp3=localUp2(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
         self.context4 = Context(in_channels, inter_channels, inter_channels, 8, norm_layer)
@@ -117,6 +117,41 @@ class Context(nn.Module):
 class localUp(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(localUp, self).__init__()
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, out_channels//2, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels//2),
+                                   nn.ReLU())
+        self.project = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels//2),
+                                   nn.ReLU())
+
+        self._up_kwargs = up_kwargs
+        self.refine = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(out_channels//2),
+                                   nn.ReLU(),
+                                    )
+        self.project2 = nn.Sequential(nn.Conv2d(out_channels//2, out_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
+                                   )
+        self.dconv = nn.Sequential(nn.ConvTranspose2d(out_channels, out_channels, 3, stride=2, padding=1, output_padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels),
+                                   nn.ReLU(),
+                                    )
+        self.relu = nn.ReLU()
+    def forward(self, c1,c2):
+        n,c,h,w =c1.size()
+        c1p = self.connect(c1) # n, 64, h, w
+        # c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
+        c2 = self.dconv(c2)
+        c2p = self.project(c2)
+        out = torch.cat([c1p,c2p], dim=1)
+        out = self.refine(out)
+        out = self.project2(out)
+        out = self.relu(c2+out)
+        return out
+    
+class localUp2(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+        super(localUp2, self).__init__()
         self.connect = nn.Sequential(nn.Conv2d(in_channels, out_channels//2, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels//2),
                                    nn.ReLU())
