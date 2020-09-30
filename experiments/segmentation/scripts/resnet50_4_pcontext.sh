4acaea0b169b808992351943f9ae4b9e6f78ebdd
yougoforward <908865817@qq.com>
2020-09-30 11:56:44 +0800

    cfpn

---
+++ experiments/segmentation/scripts/resnet50_4_pcontext.sh
@@ -0,0 +1,15 @@
+# !/usr/bin/env bash
+# train
+python -m experiments.segmentation.train_scratch --dataset pcontext \
+    --model resnet50_4 --base-size 256 --aux --crop-size 256 \
+    --checkname resnet50_4_pcontext --epochs 150
+
+#test [single-scale]
+python -m experiments.segmentation.test --dataset pcontext \
+    --model resnet50_4 --base-size 256 --aux --crop-size 256 \
+    --resume experiments/segmentation/runs/pcontext/resnet50_4/resnet50_4_pcontext/model_best.pth.tar --split val --mode testval
+
+#test [multi-scale]
+python -m experiments.segmentation.test --dataset pcontext \
+    --model resnet50_4 --base-size 256 --aux --crop-size 256 \
+    --resume experiments/segmentation/runs/pcontext/resnet50_4/resnet50_4_pcontext/model_best.pth.tar --split val --mode testval --ms
