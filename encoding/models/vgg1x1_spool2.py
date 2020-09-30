034ae2aebe9d7bb1a9aa0f549d3199fe20d8f453
yougoforward <908865817@qq.comm>
2020-09-30 20:32:53 +0800

    res50

--- encoding/models/vgg1x1_spool2.py
+++ encoding/models/vgg1x1_spool2.py
@@ -223,7 +223,7 @@ class SPool(nn.Module):
         x_w = self.conv_w2(x_w)
         
         x_h = x_h.permute(0,2,1,3)
-        x_w = x_h.permute(0,2,3,1)
+        x_w = x_w.permute(0,2,3,1)
         out = x_h+x_w
         return out
        
