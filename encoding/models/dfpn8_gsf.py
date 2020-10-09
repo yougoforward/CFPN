18c9720e4d7ce8e8c19a039c5eebd368c721b08e
yougoforward <908865817@qq.com>
2020-10-09 10:25:01 +0800

    87

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -66,7 +66,7 @@ class dfpn8_gsfHead(nn.Module):
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
 
         self.context4 = Context(in_channels, inter_channels, inter_channels, 8, norm_layer)
-        self.project4 = nn.Sequential(nn.Conv2d(2*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+        self.project4 = nn.Sequential(nn.Conv2d(3*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(inter_channels), nn.ReLU())
         self.context3 = Context(inter_channels, inter_channels, inter_channels, 8, norm_layer)
         self.project3 = nn.Sequential(nn.Conv2d(2*inter_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
@@ -81,6 +81,9 @@ class dfpn8_gsfHead(nn.Module):
     def forward(self, c1,c2,c3,c4):
         _,_, h,w = c2.size()
         cat4, p4_1, p4_8=self.context4(c4)
+        #gp
+        gp = self.gap(c4)
+        cat4 = torch.cat([p4_1, p4_8, gp.expand_as(p4_1)], dim=1)
         p4 = self.project4(cat4)
                 
         out3 = self.localUp4(c3, p4)
@@ -94,8 +97,7 @@ class dfpn8_gsfHead(nn.Module):
         p4_8 = F.interpolate(p4_8, (h,w), **self._up_kwargs)
         p3_1 = F.interpolate(p3_1, (h,w), **self._up_kwargs)
         p3_8 = F.interpolate(p3_8, (h,w), **self._up_kwargs)
-        #gp
-        gp = self.gap(c4)
+
         out = self.project(torch.cat([p2_1,p2_8,p3_1,p3_8,p4_1,p4_8, gp.expand_as(p2_1)], dim=1))
 
         #gp
