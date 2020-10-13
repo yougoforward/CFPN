102799ea85601ca37777f711c2cfa7b5f6e473d2
yougoforward <908865817@qq.com>
2020-10-13 08:32:35 +0800

    city

--- experiments/segmentation/scripts/cfpn_gsf_res101_citys2.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_citys2.sh
@@ -6,10 +6,10 @@ python -m experiments.segmentation.train --dataset citys \
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset citys \
-    --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
+    --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
     --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys2/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset citys \
-    --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
+    --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
     --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys2/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/cfpn_gsf_res101_citys4.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_citys4.sh
@@ -2,14 +2,14 @@
 # train
 python -m experiments.segmentation.train --dataset citys \
     --model cfpn_gsf --aux --base-size 1024 --crop-size 769 \
-    --backbone resnet101 --checkname cfpn_gsf_res101_citys --batch-size 8 --lr 0.02
+    --backbone resnet101 --checkname cfpn_gsf_res101_citys4 --batch-size 8 --lr 0.001
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset citys \
     --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys/model_best.pth.tar --split val --mode testval
+    --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys4/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset citys \
     --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
-    --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys/model_best.pth.tar --split val --mode testval --ms
+    --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys4/model_best.pth.tar --split val --mode testval --ms
