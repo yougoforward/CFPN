8abe11417f15258a3f0ce55d16cda7bdb5f0ed4a
yougoforward <908865817@qq.com>
2020-10-16 11:32:31 +0800

    city5

--- experiments/segmentation/scripts/cfpn_gsf_res101_citys5.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_citys5.sh
@@ -6,10 +6,10 @@
 
 #test [single-scale]
 python -m experiments.segmentation.test --dataset citys \
-    --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
+    --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
     --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys5/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test --dataset citys \
-    --model cfpn_gsf --aux --base-size 2048 --crop-size 1024 \
+    --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
     --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys5/model_best.pth.tar --split val --mode testval --ms
