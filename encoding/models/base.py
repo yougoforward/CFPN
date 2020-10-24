2e553e8b4f53da335a24a30f3d879d2f4fba01e3
yougoforward <908865817@qq.com>
2020-10-24 11:15:10 +0800

    85

--- encoding/models/base.py
+++ encoding/models/base.py
@@ -25,7 +25,7 @@ __all__ = ['BaseNet', 'MultiEvalModule', 'MultiEvalModule_whole']
 class BaseNet(nn.Module):
     def __init__(self, nclass, backbone, aux, se_loss, jpu=True, dilated=False, norm_layer=None,
                  base_size=520, crop_size=480, mean=[.485, .456, .406],
-                 std=[.229, .224, .225], root='./init_models', **kwargs):
+                 std=[.229, .224, .225], root='~/.encoding/models', **kwargs):
         super(BaseNet, self).__init__()
         self.nclass = nclass
         self.aux = aux
