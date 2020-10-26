d30efe1f9484a8829faeceeb98695fc02d1709db
yougoforward <908865817@qq.com>
2020-10-26 10:27:44 +0800

    s4

--- encoding/models/cfpn_gsf.py
+++ encoding/models/cfpn_gsf.py
@@ -94,10 +94,8 @@ class cfpn_gsfHead(nn.Module):
         se = self.se(gp)
         out = out + se*out
         out = self.gff(out)
-
         #
         out = torch.cat([out, gp.expand_as(out)], dim=1)
-
         return self.conv6(out)
 
 class Context(nn.Module):
