1a2dc17b0f3274e68aac2a2105f73ef9cd7a04a2
yougoforward <908865817@qq.comm>
2020-11-03 20:51:35 +0800

    84

--- encoding/models/dfpn84_gsf.py
+++ encoding/models/dfpn84_gsf.py
@@ -53,7 +53,7 @@ class dfpn84_gsfHead(nn.Module):
                             nn.Sigmoid())
         self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
-        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
+        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(256+inter_channels, out_channels, 1))
 
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
@@ -70,6 +70,28 @@ class dfpn84_gsfHead(nn.Module):
                                    norm_layer(inter_channels),
                                    nn.ReLU(),
                                    )
+        
+        self.project2 = nn.Sequential(nn.Conv2d(inter_channels, 256, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(256),
+                                   nn.ReLU(),
+                                   )
+        self.skip = nn.Sequential(nn.Conv2d(256, 48, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(48),
+                                   nn.ReLU(),
+                                   )
+        self.decoder = nn.Sequential(nn.Conv2d(304, 256, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(256),
+                                   nn.ReLU(),
+                                   nn.Conv2d(256, 256, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(256),
+                                   nn.ReLU(),
+                                   )
+        
+        
+        
+        
+        
+        
     def forward(self, c1,c2,c3,c4):
         _,_, h,w = c2.size()
         cat4, p4_1, p4_8=self.context4(c4)
@@ -94,8 +116,10 @@ class dfpn84_gsfHead(nn.Module):
         se = self.se(gp)
         out = out + se*out
         out = self.gff(out)
+        _,_,hl,wl = c1.size()
+        out2 = self.decoder(torch.cat([F.interpolate(self.project2(out), (hl,wl), **self._up_kwargs), self.skip(c1)], dim=1))
         #
-        out = torch.cat([out, gp.expand_as(out)], dim=1)
+        out = torch.cat([out2, gp.expand_as(out)], dim=1)
         return self.conv6(out)
 
 class Context(nn.Module):
@@ -115,15 +139,15 @@ class Context(nn.Module):
 class localUp(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
         super(localUp, self).__init__()
-        self.connect = nn.Sequential(nn.Conv2d(in_channels, in_channels//2, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(in_channels//2),
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, out_channels//2, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(out_channels//2),
                                    nn.ReLU())
         self.project = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 1, padding=0, dilation=1, bias=False),
                                    norm_layer(out_channels//2),
                                    nn.ReLU())
 
         self._up_kwargs = up_kwargs
-        self.refine = nn.Sequential(nn.Conv2d(out_channels//2+in_channels//2, out_channels//2, 3, padding=1, dilation=1, bias=False),
+        self.refine = nn.Sequential(nn.Conv2d(out_channels, out_channels//2, 3, padding=1, dilation=1, bias=False),
                                    norm_layer(out_channels//2),
                                    nn.ReLU(),
                                     )
