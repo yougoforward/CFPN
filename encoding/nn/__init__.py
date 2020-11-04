af98f4c79121d6621649febf4dcc88fbe4221280
yougoforward <908865817@qq.com>
2020-11-04 11:26:01 +0800

    dfpn84

--- encoding/nn/__init__.py
+++ encoding/nn/__init__.py
@@ -9,6 +9,13 @@
 ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 """Encoding NN Modules"""
-from .syncbn import *
 from .encoding import *
+from .syncbn import *
 from .customize import *
+from .attention import *
+from .loss import *
+from .rectify import *
+from .splat import SplAtConv2d
+from .dropblock import *
+from .encoding2 import *
+from .customize2 import *
