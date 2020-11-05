fd8db7996c8e592db63467b3f984520dd4b5c386
yougoforward <908865817@qq.comm>
2020-11-05 23:24:01 +0800

    84

--- experiments/segmentation/scripts/cfpn_gsf_res101_citys.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_citys.sh
@@ -1,13 +1,13 @@
-# # !/usr/bin/env bash
-# # train
-# python -m experiments.segmentation.train --dataset citys \
-#     --model cfpn_gsf --aux --base-size 1024 --crop-size 769 \
-#     --backbone resnet101 --checkname cfpn_gsf_res101_citys --batch-size 8 --lr 0.003  --epochs 240
+# !/usr/bin/env bash
+# train
+python -m experiments.segmentation.train --dataset citys \
+    --model cfpn_gsf --aux --base-size 1024 --crop-size 769 \
+    --backbone resnet101 --checkname cfpn_gsf_res101_citys --batch-size 8 --lr 0.002  --epochs 180
 
-# #test [single-scale]
-# python -m experiments.segmentation.test --dataset citys \
-#     --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
-#     --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys/model_best.pth.tar --split val --mode testval
+#test [single-scale]
+python -m experiments.segmentation.test --dataset citys \
+    --model cfpn_gsf --aux --base-size 2048 --crop-size 769 \
+    --backbone resnet101 --resume experiments/segmentation/runs/citys/cfpn_gsf/cfpn_gsf_res101_citys/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test --dataset citys \
