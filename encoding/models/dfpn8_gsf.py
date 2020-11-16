99720f2a07e0b47514f88de9323c972a3a8d065a
yougoforward <908865817@qq.com>
2020-11-16 16:58:34 +0800

    dfpn8_gsf

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -147,6 +147,32 @@ class localUp(nn.Module):
         return out
 
 
+class localUp2(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+        super(localUp2, self).__init__()
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, 32, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(32),
+                                   nn.ReLU())
+        self.att = nn.Sequential(nn.Conv2d(out_channels+32, 1, 1, padding=0, dilation=1, bias=True),
+                                   nn.Sigmoid())
+
+        self._up_kwargs = up_kwargs
+        self.refine = nn.Sequential(nn.Conv2d(out_channels+32, 256, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(256),
+                                   nn.ReLU(),
+                                   nn.Conv2d(256, 256, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(256),
+                                   nn.ReLU(),
+                                    )
+    def forward(self, c1,c2):
+        n,c,h,w =c1.size()
+        c1p = self.connect(c1) # n, 64, h, w
+        c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
+        attention = self.att(torch.cat([c1p,c2], dim=1))
+        out = torch.cat([c1p*attention,c2], dim=1)
+        out = self.refine(out)
+        return out
+    
 def get_dfpn8_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
     # infer number of classes
