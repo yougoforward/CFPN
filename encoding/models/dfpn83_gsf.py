83d0b5bc50ddf4f582c84418c73dd4778b2fd30a
yougoforward <908865817@qq.com>
2020-11-16 20:30:02 +0800

    up

--- encoding/models/dfpn83_gsf.py
+++ encoding/models/dfpn83_gsf.py
@@ -53,7 +53,7 @@ class dfpn83_gsfHead(nn.Module):
                             nn.Sigmoid())
         self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
-        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(2*inter_channels, out_channels, 1))
+        self.conv6 = nn.Sequential(nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
 
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
