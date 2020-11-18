c144dcd222da71c8dad663c3567c4ff288e3997d
yougoforward <908865817@qq.com>
2020-11-18 09:36:14 +0800

    up

--- encoding/models/dfpn83_gsf.py
+++ encoding/models/dfpn83_gsf.py
@@ -49,13 +49,12 @@ class dfpn83_gsfHead(nn.Module):
                             norm_layer(inter_channels),
                             nn.ReLU(True))
         self.se = nn.Sequential(
-                            nn.Conv2d(inter_channels, inter_channels, 1, bias=True),
+                            nn.Conv2d(2*inter_channels, inter_channels, 1, bias=True),
                             nn.Sigmoid())
         self.gff = PAM_Module(in_dim=inter_channels, key_dim=inter_channels//8,value_dim=inter_channels,out_dim=inter_channels,norm_layer=norm_layer)
 
-        self.conv6 = nn.Sequential(nn.Conv2d(2*inter_channels, 256, 1, padding=0, dilation=1, bias=False),
-                                   norm_layer(256), nn.ReLU(),
-                                   nn.Dropout2d(0.1), nn.Conv2d(256, out_channels, 1))
+        self.conv6 = nn.Sequential(
+                                   nn.Dropout2d(0.1), nn.Conv2d(inter_channels, out_channels, 1))
 
         self.localUp3=localUp(512, inter_channels, norm_layer, up_kwargs)
         self.localUp4=localUp(1024, inter_channels, norm_layer, up_kwargs)
@@ -93,11 +92,11 @@ class dfpn83_gsfHead(nn.Module):
         #gp
         gp = self.gap(c4)    
         # se
-        se = self.se(gp)
+        se = self.se(torch.cat([out, gp.expand_as(out)], dim=1))
         out = out + se*out
         out = self.gff(out)
         #
-        out = torch.cat([out, gp.expand_as(out)], dim=1)
+        # out = torch.cat([out, gp.expand_as(out)], dim=1)
         return self.conv6(out)
 
 class Context(nn.Module):
