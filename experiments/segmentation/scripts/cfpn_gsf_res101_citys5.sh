4e43438d5c7c5781eb16bb52ec397a8647de2ca4
yougoforward <908865817@qq.comm>
2020-11-27 01:01:05 +0800

    city

--- experiments/segmentation/scripts/cfpn_gsf_res101_citys5.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_citys5.sh
@@ -2,7 +2,7 @@
 # train
 python -m experiments.segmentation.train --dataset citys \
     --model cfpn_gsf --aux --base-size 1024 --crop-size 769 \
-    --backbone resnet101 --checkname cfpn_gsf_res101_citys5 --batch-size 8 --lr 0.001 --epochs 120
+    --backbone resnet101 --checkname cfpn_gsf_res101_citys5 --batch-size 8 --lr 0.002 --epochs 180
 
 #test [single-scale]
 python -m experiments.segmentation.test --dataset citys \
