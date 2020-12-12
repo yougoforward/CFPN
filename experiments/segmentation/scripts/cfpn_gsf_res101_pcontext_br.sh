b8247043c46fb1b51e923aa91dbf13ed6bd2cb5b
yougoforward <908865817@qq.com>
2020-12-12 12:37:16 +0800

    obj

--- experiments/segmentation/scripts/cfpn_gsf_res101_pcontext_br.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_pcontext_br.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 # train
-python -m experiments.segmentation.train_br --dataset pcontext_br \
+python -m experiments.segmentation.train_br --dataset pcontext \
     --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
     --backbone resnet101 --checkname cfpn_gsf_res101_pcontext_br
 
--- experiments/segmentation/scripts/cfpn_gsf_res101_pcontext_br3.sh
+++ experiments/segmentation/scripts/cfpn_gsf_res101_pcontext_br3.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 # train
-python -m experiments.segmentation.train_br3 --dataset pcontext3 \
+python -m experiments.segmentation.train_br3 --dataset pcontext \
     --model cfpn_gsf --aux --base-size 520 --crop-size 520 \
     --backbone resnet101 --checkname cfpn_gsf_res101_pcontext_br3
 
