0d8d1dff583d16e6ac6ad6fe10069a81e38b5a1e
yougoforward <908865817@qq.com>
2020-12-12 11:34:28 +0800

    obj

--- encoding/nn/customize.py
+++ encoding/nn/customize.py
@@ -23,7 +23,7 @@ from .BoundaryLabelRelaxationLoss import ImgWtLossSoftNLL
 
 torch_ver = torch.__version__[:3]
 
-__all__ = ['SegmentationLosses', 'SegmentationLosses_contour', 'SegmentationLosses_contour_BoundaryRelax', 'PyramidPooling', 'JPU', 'JPU_X', 'Mean', 'SegmentationLosses_object', 'SegmentationLosses_objectcut', 'SegmentationLosses_BoundaryRelax','SegmentationLosses_BoundaryRelax3'，'SegmentationLosses_BoundaryRelax5']
+__all__ = ['SegmentationLosses', 'SegmentationLosses_contour', 'SegmentationLosses_contour_BoundaryRelax', 'PyramidPooling', 'JPU', 'JPU_X', 'Mean', 'SegmentationLosses_object', 'SegmentationLosses_objectcut', 'SegmentationLosses_BoundaryRelax','SegmentationLosses_BoundaryRelax3','SegmentationLosses_BoundaryRelax5']
 
 class FocalLoss(nn.Module):
     def __init__(self, gamma=0, alpha=None, size_average=True):
