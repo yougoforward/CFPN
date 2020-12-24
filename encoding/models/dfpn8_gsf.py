320e1ea9dad2c457b2013b26e5ad13601064afc0
yougoforward <908865817@qq.com>
2020-12-24 21:13:32 +0800

    city

--- encoding/models/dfpn8_gsf.py
+++ encoding/models/dfpn8_gsf.py
@@ -68,6 +68,9 @@ class dfpn8_gsfHead(nn.Module):
         
         self.localsr1=localSR(256, out_channels, up_kwargs)
         self.localsr0=localSR(128, out_channels, up_kwargs)
+        
+        self.contour1 = Boundary(256, inter_channels, norm_layer, up_kwargs)
+
     def forward(self, x,c0,c1,c2,c3,c4):
         _,_, h,w = c2.size()
         cat4, p4_1, p4_8=self.context4(c4)
@@ -97,7 +100,8 @@ class dfpn8_gsfHead(nn.Module):
         out = torch.cat([out, gp.expand_as(out)], dim=1)
         out = self.conv6(out)
         
-        out1 = self.localsr1(c1, out)
+        edge1 = self.contour1(c1, out)
+        out1 = self.localsr1(edge1, out)
         return [out1, out]
         # out2 = self.localsr0(c0, out1)
 
@@ -151,11 +155,11 @@ class localSR(nn.Module):
     def __init__(self, in_channels, out_channels, up_kwargs):
         super(localSR, self).__init__()
         inter_channels = 64
-        self.connect = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=True),
-                                   nn.ReLU())
+        # self.connect = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=True),
+        #                            nn.ReLU())
 
         self._up_kwargs = up_kwargs
-        self.refine = nn.Sequential(nn.Conv2d(inter_channels+out_channels, inter_channels, 3, padding=1, dilation=1, bias=True),
+        self.refine = nn.Sequential(nn.Conv2d(in_channels+out_channels, inter_channels, 3, padding=1, dilation=1, bias=True),
                                    nn.ReLU(),
                                    nn.Conv2d(inter_channels, inter_channels, 3, padding=1, dilation=1, bias=True),
                                    nn.ReLU(),
@@ -164,14 +168,45 @@ class localSR(nn.Module):
                                    )
     def forward(self, c1,c2):
         n,c,h,w =c1.size()
-        c1p = self.connect(c1) # n, 64, h, w
+        # c1p = self.connect(c1) # n, 64, h, w
         c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
-        out = torch.cat([c1p,c2], dim=1)
+        out = torch.cat([c1,c2], dim=1)
         out = self.refine(out)
         out = self.project2(out)
-        out = c2+out
+        out = c2+outboundary
         return out
     
+class Boundary(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer, up_kwargs):
+        super(Boundary, self).__init__()
+        inter_channels = 32
+        self.connect = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                     norm_layer(inter_channels),
+                                   nn.ReLU())
+        self.project = nn.Sequential(nn.Conv2d(out_channels, inter_channels, 1, padding=0, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU())
+
+        self._up_kwargs = up_kwargs
+        self.refine = nn.Sequential(nn.Conv2d(inter_channels*2, inter_channels, 3, padding=1, dilation=1, bias=False),
+                                    norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                   nn.Conv2d(inter_channels, inter_channels, 3, padding=1, dilation=1, bias=False),
+                                   norm_layer(inter_channels),
+                                   nn.ReLU(),
+                                    )
+        self.project2 = nn.Sequential(nn.Conv2d(inter_channels, 1, 1, padding=0, dilation=1, bias=True),
+                                      nn.Sigmoid()
+                                   )
+    def forward(self, c1,c2):
+        n,c,h,w =c1.size()
+        c1p = self.connect(c1) # n, 32, h, w
+        c2 = F.interpolate(c2, (h,w), **self._up_kwargs)
+        c2p = self.project(c2)
+        out = torch.cat([c1p,c2p], dim=1)
+        out = self.refine(out)
+        out = self.project2(out)
+        return out
 
 def get_dfpn8_gsf(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                  root='~/.encoding/models', **kwargs):
