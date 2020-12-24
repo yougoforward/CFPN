a177ccdbb1098ca40fd1eaf84411f89786a9314b
yougoforward <908865817@qq.com>
2020-12-24 10:18:59 +0800

    city

--- encoding/models/dpcan.py
+++ encoding/models/dpcan.py
@@ -40,8 +40,7 @@ class dpcanHead(nn.Module):
     def __init__(self, in_channels, out_channels, norm_layer, up_kwargs, atrous_rates):
         super(dpcanHead, self).__init__()
         inter_channels = in_channels // 4
-        self.aspp1 = ASPP_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
-        self.aspp2 = ASPP_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
+        self.aspp = ASPP_Module(in_channels, inter_channels, atrous_rates, norm_layer, up_kwargs)
 
         self._up_kwargs = up_kwargs
         
@@ -64,8 +63,7 @@ class dpcanHead(nn.Module):
         out3 = self.localUp4(c3, c4)  
         out = self.localUp3(c2, out3)
         #dual path
-        aspp1 = self.aspp1(out)
-        aspp2 = self.aspp2(out)
+        aspp1, aspp2 = self.aspp(out)
 
         #class-aware attention
         concat = torch.cat([aspp1, aspp2], dim=1)
@@ -151,7 +149,12 @@ class ASPP_Module(nn.Module):
         #     norm_layer(out_channels),
         #     nn.ReLU(True),
         #     nn.Dropout2d(0.5, False))
-        self.project = nn.Sequential(
+        self.project1 = nn.Sequential(
+            nn.Conv2d(5*out_channels, out_channels, 1, bias=False),
+            norm_layer(out_channels),
+            nn.ReLU(True))
+        
+        self.project2 = nn.Sequential(
             nn.Conv2d(5*out_channels, out_channels, 1, bias=False),
             norm_layer(out_channels),
             nn.ReLU(True))
@@ -164,8 +167,10 @@ class ASPP_Module(nn.Module):
         feat4 = self.b4(x)
 
         y = torch.cat((feat0, feat1, feat2, feat3, feat4), 1)
+        aspp1 = self.project1(y)
+        aspp2 = self.project2(y)
 
-        return self.project(y)
+        return aspp1, aspp2
 
 def get_dpcan(dataset='pascal_voc', backbone='resnet50', pretrained=False,
                 root='~/.encoding/models', **kwargs):
